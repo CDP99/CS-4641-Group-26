c4e1ad79dd341b5701ab2565a9b2ecf01c2f63c0
Matthew Nilsen <mnilsen50@gmail.com>
2021-03-26 01:03:28 -0400

    Growths, Values, Ratios

--- API.py
+++ API.py
@@ -2,11 +2,16 @@ import requests
 import pandas as pd
 
 class API:
-    def __init__(self, key):
+    def __init__(self, key, period='annual', limit=5):
         self.base_url = 'https://financialmodelingprep.com/'
         self.params = {
-            'apikey': key
+            'apikey': key,
+            'period': period,
+            'limit': limit,
+            'from': '2015-01-01',
+            'to': '2018-01-01'
         }
+        self.alwaysCols = ["symbol", "date"]
 
     def request(self, url):
         r = requests.get(url, params=self.params)
@@ -24,9 +29,47 @@ class API:
         except ValueError:
             raise Exception("Bad URL or API Key!")
 
-        df = df.reindex(index=df.index[::-1])
+        # df = df.reindex(index=df.index[::-1])
+        df.sort_values(by=['date'], inplace=True, ascending=True)
+        df.reset_index(drop=True, inplace=True)
         return df
 
     def getRatios(self, ticker):
         endpoint = self.base_url + 'api/v3/ratios/' + ticker
-        return self.getDataFrame(endpoint)
+        wantedCols = [
+            "currentRatio", "quickRatio", "grossProfitMargin", "operatingProfitMargin", "returnOnAssets",
+            "returnOnEquity", "returnOnCapitalEmployed", "debtEquityRatio", "priceToBookRatio", "priceToSalesRatio",
+            "priceEarningsRatio", "priceToOperatingCashFlowsRatio" 
+        ]
+        df = self.getDataFrame(endpoint)
+        df = df[self.alwaysCols + wantedCols]
+        return df
+
+    def getGrowths(self, ticker):
+        endpoint = self.base_url + 'api/v3/income-statement-growth/' + ticker
+        wantedCols = [
+            "growthRevenue", "growthCostOfRevenue", "growthGrossProfit", "growthGrossProfitRatio",
+            "growthOperatingExpenses", "growthEBITDA", "growthOperatingIncome", "growthNetIncome",
+            "growthEPS"
+        ]
+        df = self.getDataFrame(endpoint)
+        df = df[self.alwaysCols + wantedCols]
+        return df
+
+    def getValues(self, ticker):
+        endpoint = self.base_url + 'api/v3/enterprise-values/' + ticker
+        wantedCols = [
+            "marketCapitalization", "enterpriseValue"
+        ]
+        df = self.getDataFrame(endpoint)
+        df = df[self.alwaysCols + wantedCols]
+        return df
+
+    def getSamples(self, ticker):
+        ratios = self.getRatios(ticker)
+        growths = self.getGrowths(ticker)
+        values = self.getValues(ticker)
+
+        merge = pd.concat([ratios, growths, values], axis=1)
+
+        return merge
