0342bd1359d4f6f55c52dd1315f49241cc3f83bd
Matthew Nilsen <mnilsen50@gmail.com>
2021-04-18 23:28:59 -0400

    Regression working (but not well)

--- API.py
+++ API.py
@@ -1,5 +1,8 @@
 import requests
 import pandas as pd
+import random
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 class API:
     def __init__(self, key, period='annual', limit=5):
@@ -11,6 +14,9 @@ class API:
         }
         self.alwaysCols = ["symbol", "date"]
 
+        if limit == None:
+            del self.params['limit']
+
     def request(self, url):
         r = requests.get(url, params=self.params)
 
@@ -19,13 +25,21 @@ class API:
 
         return r
 
-    def getDataFrame(self, endpoint): 
+    def getDataFrameUnordered(self, endpoint):
         r = self.request(endpoint)
 
         try:
             df = pd.json_normalize(r.json())
         except ValueError:
             raise Exception("Bad URL or API Key!")
+
+        return df
+
+    def getDataFrame(self, endpoint): 
+        df = self.getDataFrameUnordered(endpoint)
+
+        if df.empty:
+            raise ValueError("Error, empty dataframe")
 
         # df = df.reindex(index=df.index[::-1])
         df.sort_values(by=['date'], inplace=True, ascending=True)
@@ -78,3 +92,78 @@ class API:
         merge = pd.merge(merge, values, how='inner', left_on=["date", "symbol"], right_on=["date", "symbol"])
 
         return merge
+
+    def getRandTickers(self, num):
+        endpoint = self.base_url + 'api/v3/available-traded/list'
+        tickers = pd.DataFrame(self.request(endpoint).json())
+        rowIdx = random.sample(range(0, len(tickers)), num)
+
+        return list(tickers.iloc[rowIdx]['symbol'])
+
+    def getSamplesFromTickers(self, tickers, samplesPerTicker=5, maxTickers=10):
+        '''
+        tickers: list Creates dataframe of samples from list of tickers
+        samplesPerTicker: int Number of rows of data to get per ticker
+        '''
+        result = pd.DataFrame()
+        i = 0
+
+        for t in tickers:
+            if i >= maxTickers:
+                break
+
+            try:
+                oneYearAgo = (datetime.today() - relativedelta(years=1, months=6)).strftime('%Y-%m-%d')
+                df = self.getSamples(t)
+                df = df[df['date'] < oneYearAgo]
+                rowIdx = random.sample(range(0, len(df)), samplesPerTicker)
+            except ValueError:
+                # Ticker didn't have enough data
+                continue
+
+            samples = df.iloc[rowIdx]
+            result = result.append(pd.DataFrame(samples))
+            i += 1
+
+        return result
+
+    def getYFromDF(self, X):
+        groups = X.groupby(['symbol'])
+        result = pd.DataFrame()
+
+        for state, frame in groups:
+            dates = list(frame['date'])
+            try:
+                priceChange = self.getPriceChange(state, dates)
+            except ValueError:
+                # No price data for stock
+                continue
+
+            result = result.append(priceChange)
+
+        return result
+
+    def getPriceChange(self, ticker, dates):
+        endpoint = self.base_url + 'api/v3/historical-price-full/' + ticker
+        self.params['serietype'] = 'line'
+        r = self.request(endpoint)
+        del self.params['serietype']
+
+        if not r.json():
+            raise ValueError("No price data for stock")
+
+        addOneYear = lambda d: (datetime.strptime(d, "%Y-%m-%d") + relativedelta(years=1)).strftime('%Y-%m-%d')
+        subOneYear = lambda d: (datetime.strptime(d, "%Y-%m-%d") - relativedelta(years=1)).strftime('%Y-%m-%d')
+
+        futureDates = [addOneYear(d) for d in dates]
+        priceData = [o for o in r.json()['historical'] if o['date'] in futureDates]
+        
+        endData = {'symbol': [], 'date': [], 'futureDate': [], 'futureClose': []}
+
+        for o in priceData:
+            endData['date'].append(subOneYear(o['date']))
+            endData['futureDate'].append(o['date'])
+            endData['futureClose'].append(o['close'])
+            endData['symbol'].append(ticker)
+
+        return pd.DataFrame(endData)
--- test.py
+++ test.py
@@ -1,6 +1,30 @@
+import os
 import requests
+import numpy as np
 import pandas as pd
 
-r = requests.get('https://financialmodelingprep.com/api/v3/historical-price-full/AAPL?serietype=line&apikey=b903b84d1ce1092d3e9fca34e25bf408')
-a = r.json()
-print(pd.DataFrame(a))
+from sklearn.decomposition import PCA, FastICA
+from sklearn.preprocessing import StandardScaler, scale, MinMaxScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import KFold, cross_val_score
+
+import statsmodels.tsa.api as tsa
+from scipy.stats import probplot, moment
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from api_keys import fmp_api_key as api_key
+from API import API
+
+api = API(api_key, period='quarter', limit=None)
+tickers = api.getRandTickers(50)
+
+X = api.getSamplesFromTickers(tickers, maxTickers=5)
+Y = api.getYFromDF(X)
+
+merge = pd.merge(X, Y, how='inner', left_on=["date", "symbol"], right_on=["date", "symbol"])
+X = merge.drop(['date', 'symbol', 'futureDate', 'futureClose'], axis=1).to_numpy()
+Y = merge[['futureClose']].to_numpy()
+
+print()
